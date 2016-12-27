48506fe18955043a6d6a19f117c7efcb2e40d592
zhanghao639 <zhanghao639@pingan.com.cn>
2016-12-27 16:27:54 +0800

    fixed

--- sh/release.sh
+++ sh/release.sh
@@ -124,7 +124,7 @@ do
         if [[ $branch != "master" ]]
         then
             echo -e "当前操作分支非master分支，进行删除本地分支操作\n"
-            execCommand $dir "git branch -D ${branch}"
+            `git branch -D ${branch} 2>&1`
         fi
     fi
 done
